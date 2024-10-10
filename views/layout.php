c5d03b212c9ef6fe325ce528e9fe490985e1d59c
Manuel Lucero <manu.lucero07@gmail.com>
2024-10-10 10:13:30 -0300

    Con relativa

--- views/layout.php
+++ views/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario Andanatura</title>
-    <link rel="stylesheet" href="../src/scss/app.scss">
+    <link rel="stylesheet" href="../public/build/css/app.css">
 </head>
 <body>
     
